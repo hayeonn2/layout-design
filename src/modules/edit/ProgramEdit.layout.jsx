41e852617d3cd0f14fc593410b9759234d38ae67
hayeonn2 <dwd1205@naver.com>
2024-05-04 16:28:41 +0900

    feat: 프로그램 개설 레이아웃 생성

--- src/modules/edit/ProgramEdit.layout.jsx
+++ src/modules/edit/ProgramEdit.layout.jsx
@@ -1,3 +1,13 @@
+import { Title } from "../common/components/Title";
+import "./ProgramEdit.layout.css";
+
 export const ProgramEditLayout = () => {
-  return <div>프로그램 개설해버리는 곳</div>;
+  return (
+    <div className="program_edit_layout">
+      <Title
+        title={"프로그램 개설"}
+        comment={"멋진 프로그램을 개설해보려고 합니다."}
+      />
+    </div>
+  );
 };
