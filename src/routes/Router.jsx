593d6c5a00313d0746c17646517b81bcad19d623
hayeonn2 <dwd1205@naver.com>
2024-05-05 16:29:02 +0900

    feat: Program 라우터 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProgramList } from "../modules/main/ProgramList";
-import { ProgramEditLayout } from "../modules/edit/ProgramEdit.layout";
+import { ProgramEditLayout } from "../modules/edit/layout/ProgramEdit.layout";
+import { ProgramEdit } from "../modules/edit/main/ProgramEdit";
 
 const Router = () => {
   return (
@@ -8,7 +9,10 @@ const Router = () => {
       <Routes>
         {/* 제일 메인 페이지 */}
         <Route exact path="/" element={<ProgramList />} />
-        <Route path="edit" element={<ProgramEditLayout />} />
+
+        <Route path="edit/*" element={<ProgramEditLayout />}>
+          <Route index element={<ProgramEdit />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
