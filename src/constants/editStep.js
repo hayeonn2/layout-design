510390f6f8566d9d84229b5495c7de45a124fdbf
hayeonn2 <dwd1205@naver.com>
2024-05-05 16:27:52 +0900

    feat: 개설 단계 step 상수 editStep 생성

---
+++ src/constants/editStep.js
@@ -0,0 +1,18 @@
+import Step1 from "../assets/svg/step_1.svg";
+import Step2 from "../assets/svg/step_2.svg";
+
+// 개설 단계 step 상수 데이터
+export const EDIT_STEP = [
+  {
+    img: Step1,
+    alt: "프로그램 정보 입력 아이콘",
+    step: "01",
+    title: "프로그램 정보 입력",
+  },
+  {
+    img: Step2,
+    alt: "프로그램 설계 아이콘",
+    step: "02",
+    title: "프로그램 설계",
+  },
+];
