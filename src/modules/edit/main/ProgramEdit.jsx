6e5d850fcb0f42f7feff9ca3f0c95d1a128e34d2
hayeonn2 <dwd1205@naver.com>
2024-05-06 22:48:39 +0900

    feat: programRef 전달 테스트

--- src/modules/edit/main/ProgramEdit.jsx
+++ src/modules/edit/main/ProgramEdit.jsx
@@ -1,14 +1,31 @@
 import { useEffect } from "react";
 import { useOutletContext, useSearchParams } from "react-router-dom";
+import { ProgramStep1 } from "../components/step/Program.step1";
 
 export const ProgramEdit = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const { programRef, formData } = useOutletContext();
+
+  // Outlet에 기본 제공되는 context를 이용해 자식요소에게 Props 전달
+  const { programRef } = useOutletContext();
 
   useEffect(() => {
     searchParams.set("step", "1");
     setSearchParams(searchParams);
   }, []);
 
-  return <div>자식요소에염~!</div>;
+  return (
+    <div
+      style={{
+        position: "fixed",
+        left: "50%",
+        top: "45%",
+        padding: "30px",
+        color: "white",
+        backgroundColor: "black",
+      }}
+      onClick={() => console.log(programRef)}
+    >
+      <ProgramStep1 programRef={programRef} />
+    </div>
+  );
 };
